e4a6b2ddb3e62055b700a2e1d228e6a5f1101727
osgun4christ <osgun4christ@gmail.com>
2021-06-22 23:43:58 +0000

    onboarding page 1 xml complete

--- app/src/main/java/com/example/calorify/OnBoardingPage1.kt
+++ app/src/main/java/com/example/calorify/OnBoardingPage1.kt
@@ -3,9 +3,9 @@ package com.example.calorify
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class OnBoardingPage1 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_onboardingpage1)
     }
 }
